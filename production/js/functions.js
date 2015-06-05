d58aac00d39027c06ed2fb2a5cea3f22386764f7
slaterjen <js492683@E224L64.local>
2015-06-05 15:45:57 -0500

    Production-Update-6.5.15
    Worked on responsive images, and trying to figure out links to social
    media.

---
+++ production/js/functions.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+jQuery(document).ready(function($){
+	
+	// Smooth scroll home nav
+		$('#smooth-scroll').on('click', 'a', function(e) {
+			// prevent default anchor click behavior
+   			e.preventDefault();
+		
+		   // store hash
+		   var hash = this.hash;
+			
+			$('html, body').animate({
+			   scrollTop: $(this.hash).offset().top-170
+			 }, 500, function(){
+		
+			   // when done, add hash to url
+			   // (default click behaviour)
+			   window.location.hash = hash;
+			 });
+		
+		});
+
+	
+});
